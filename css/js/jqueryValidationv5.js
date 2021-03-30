989f5d96b4d347cde12723b2c3f8349a0b313410
hwtcnb <hwtcnb@gmail.com>
2021-03-30 15:10:23 +0300

    Create jqueryValidationv5.js

---
+++ css/js/jqueryValidationv5.js
@@ -0,0 +1,129 @@
+function formJSSend(form) {
+    const dataServer = document.createElement('p')
+    dataServer.classList.add('dataServer')
+    document.querySelector('.response-content').append(dataServer)
+
+    const request = new XMLHttpRequest()
+
+    request.open('POST', 'server.php')
+    request.setRequestHeader('Content-Type', 'application/json; charset=utf-8')
+    
+    const formData = new FormData(form)
+
+    const data_obj = {}
+
+    formData.forEach((value, key) => {
+        data_obj[key] = value
+    })
+
+    const data = JSON.stringify(data_obj)
+
+    request.send(data)
+
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+          dataServer.innerHTML = request.response
+          form.reset()
+          setTimeout(() => {
+            dataServer.innerHTML = ''
+            document.getElementById('response-popup').style.display = 'none'
+          }, 10000)
+        } else {
+          dataServer.innerHTML = 'Fail'
+        }
+      })
+}
+
+
+
+function formJQuerySend(form){
+
+    const dataServer = document.createElement('p')
+    dataServer.classList.add('dataServer')
+    document.querySelector('.response-content').append(dataServer)
+
+
+    const formData = new FormData(form)
+
+    const data_obj = {}
+
+    formData.forEach((value, key) =>{
+        data_obj[key] = value
+    })
+
+    $.ajax({
+        url: `server.php`,
+        type: "POST", 
+        contentType: "application/json",
+        data: JSON.stringify(data_obj),
+        success: (data, textStatus) => {
+            if (textStatus === 'success') {
+                dataServer.innerHTML = data
+                form.reset()
+                setTimeout(() => {
+            dataServer.innerHTML = ''
+            document.getElementById('response-popup').style.display = 'none'
+          }, 10000)
+            }
+        },
+        error: (jqXHR, textStatus, errorThrown) => {
+            console.log(textStatus)
+            console.log(errorThrown)
+            dataServer.innerHTML = 'Fail'
+        }
+    })
+}
+
+
+
+$('.validate').validate({
+    rules: {
+        name: {
+            required: true,
+            minlength: 2 
+        },
+        email:{
+            required: true,
+            pattern: /^[a-z0-9]+@[a-z]+\.[a-z]{2,4}$/
+        },
+        surname:{
+            minlength: 2,
+            required: true
+        },
+        pass:{
+            required: true,
+            minlength: 6
+        }
+    },
+
+    messages:{
+        name:{
+            required: "Поле не повинно бути порожнім!",
+            minlength: "Занадто коротке ім'я!"
+        },
+        email:{
+            required: "Поле не повинно бути порожнім!",
+            pattern: "Введіть пошту згідно формату!"
+        },
+        surname:{
+            required: "Поле не повинно бути порожнім!",
+            minlength: "Занадто коротке прізвище!"
+        },
+        pass:{
+            required: "Поле не повинно бути порожнім!",
+            minlength: "Пароль має містити мінімум 6 символів!"
+        }
+    },
+
+    submitHandler: function (form) {
+        const chooser = prompt('Enter "jq" if you want jQuery_send, enter "js" if you want JavaScript_send')
+
+        if (chooser === 'jq') {
+            formJQuerySend(form)
+        }
+        else{
+            formJSSend(form)
+        }
+        document.getElementById('response-popup').style.display = 'block'
+    }
+})

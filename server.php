4ad930c29f6dcfbbd3e4d498a5dded76b9a70ef1
hwtcnb <hwtcnb@gmail.com>
2021-03-31 14:01:41 +0300

    Update server.php

--- server.php
+++ server.php
@@ -1,3 +1,25 @@
 <?php
     $_POST = json_decode(file_get_contents('php://input'), true);
-    echo var_dump($_POST);
+    //echo var_dump($_POST);
+    class User
+{
+    public function __construct($name, $surname)
+    {
+        $this->name = $name;
+        $this->surname = $surname;
+        //$this->father = $father;
+        //$this->birthday = $birthday;
+    }
+    
+    public function print_data()
+    {
+        echo $this->name . '</br>';
+        echo $this->surname;
+    }
+}
+
+$user = new User($_POST["name"], $_POST["surname"]);
+
+$user->print_data();
+
+//print htmlspecialchars($_POST["name"]);
